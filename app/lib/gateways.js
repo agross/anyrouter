df3979980471f7d2524345bf01fb457e6df06444
Alexander Groß <agross@therightstuff.de>
2019-10-10 02:59:28 +0200

    Move router to app

--- app/lib/gateways.js
+++
@@ -1,7 +0,0 @@
-let gateways = (process.env.GATEWAYS || '').split(',')
-                                          .filter(gw => gw.length > 0);
-if (gateways.length === 0) {
-  throw 'No gateways defined as GATEWAYS environment variable.';
-}
-
-module.exports.GATEWAYS = gateways
