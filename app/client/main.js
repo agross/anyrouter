c67f2ccd5f7f3cbcdf5834910009b81319c9abee
Alexander Groß <agross@therightstuff.de>
2019-10-10 14:13:23 +0200

    Display status instead of appending event to a list

--- app/client/main.js
+++ app/client/main.js
@@ -1,24 +1,41 @@
+Vue.use(vueMoment);
+
 const app = new Vue({
   el: '#app',
   data: {
-    title: 'Router Config',
-    name: '',
-    text: '',
-    messages: [],
-    socket: null
+    title: 'anyrouter',
+    socket: null,
+    status: null,
+    exception: null,
+    events: {}
   },
   methods: {
-    sendMessage() {
-      this.socket.emit('msgToServer', message)
+    sendMessage: function() {
+      this.socket.emit('msgToServer', message);
     },
-    receivedMessage(message) {
-      this.messages.push(message);
+    setStatus: function (status, exception) {
+      this.status = status;
+      this.exception = exception;
     },
+    receivedEvent: function (event) {
+      event.timestamp = new Date(event.timestamp);
+      Vue.set(this.events, event.type + event.data, event);
+    }
   },
   created() {
     this.socket = io('/');
-    this.socket.on('events', (message) => {
-      this.receivedMessage(message);
+    this.socket.on('connect', () => {
+      this.setStatus('connected');
+    });
+    this.socket.on('exception', data => {
+      this.status = 'exception';
+      this.exception = data;
+    });
+    this.socket.on('disconnect', () => {
+      this.setStatus('disconnected');
+    });
+    this.socket.on('events', event => {
+      this.receivedEvent(event);
     });
   }
 });
--- app/src/events/events.gateway.ts
+++ app/src/events/events.gateway.ts
@@ -18,9 +18,15 @@ export class EventsGateway implements OnGatewayInit {
   afterInit(server: any) {
     this.queue.on(
       BullQueueGlobalEvents.COMPLETED,
-      async (id, result, _status) => {
+      async (id, _result, _status) => {
         const job = await this.queue.getJob(id);
-        this.onEvent({ type: job.name, data: job.data, result: result });
+        this.onEvent({
+          type: job.name,
+          data: job.data,
+          timestamp: job.processedOn,
+          result: job.returnvalue
+        });
+
         await job.remove();
       }
     );
