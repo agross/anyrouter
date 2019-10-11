b37f83d7d06cd99930ffb1ea6fb9ce3d79650cf0
Alexander Groß <agross@therightstuff.de>
2019-10-11 17:58:23 +0200

    Extract process runner into a function

--- app/src/jobs/worker/set-default-gateway.ts
+++ app/src/jobs/worker/set-default-gateway.ts
@@ -3,51 +3,56 @@ import { Logger } from '@nestjs/common';
 import * as path from 'path';
 import { spawn } from 'child_process';
 
+async function runProcess(
+  logger: Logger,
+  command: string,
+  args?: readonly string[]
+): Promise<string> {
+  return new Promise((resolve, reject) => {
+    logger.debug(`Running ${command} ${args}`);
+    const process = spawn(command, args);
+
+    process.on('error', error => {
+      logger.error(error.message, error.stack);
+      reject(error.stack);
+    });
+
+    let output = '';
+    process.stdout.on('data', chunk => {
+      output += chunk.toString();
+    });
+    process.stderr.on('data', chunk => {
+      output += chunk.toString();
+    });
+
+    process.on('exit', code => {
+      if (code == 0) {
+        logger.log(`Status: ${code}`);
+        logger.debug(`Output: ${output}`);
+
+        resolve(output);
+      } else {
+        logger.error(`Status: ${code}`);
+        logger.error(`Output: ${output}`);
+
+        reject(`[${code}] ${output}`);
+      }
+    });
+  });
+}
+
 export default async function(job: Job) {
-  const logger = new Logger(`${path.basename(__filename, path.extname(__filename))} worker`);
+  const logger = new Logger(
+    `${path.basename(__filename, path.extname(__filename))} worker`
+  );
   logger.log(`${job.data.description} ${job.id}`);
 
-  await new Promise((resolve, reject) => {
-    const process = spawn('ip', ['route', 'del', 'default']);
-
-    let output = '';
-    process.stdout.on('data', chunk => {
-      output += chunk.toString();
-    });
-
-    process.on('exit', code => {
-      console.log(`Exit code is: ${code}`);
-
-      if (code == 0) {
-        resolve(code + ' ' + output);
-      } else {
-        reject(code + ' ' + output);
-      }
-    });
-  });
-
-  return new Promise((resolve, reject) => {
-    const process = spawn('ip', [
-      'route',
-      'add',
-      'default',
-      'via',
-      job.data.gateway
-    ]);
-
-    let output = '';
-    process.stdout.on('data', chunk => {
-      output += chunk.toString();
-    });
-
-    process.on('exit', code => {
-      console.log(`Exit code is: ${code}`);
-
-      if (code == 0) {
-        resolve(code + ' ' + output);
-      } else {
-        reject(code + ' ' + output);
-      }
-    });
-  });
+  await runProcess(logger, 'ip', ['route', 'del', 'default']);
+  await runProcess(logger, 'ip', [
+    'route',
+    'add',
+    'default',
+    'via',
+    job.data.gateway
+  ]);
 }
