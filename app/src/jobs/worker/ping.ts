18b1924ff447bb81058e63726781f1050a407f1a
Alexander Groß <agross@therightstuff.de>
2019-10-13 20:07:10 +0200

    Replace is-reachable with net-ping

--- app/src/jobs/worker/ping.ts
+++ app/src/jobs/worker/ping.ts
@@ -1,7 +1,8 @@
 import { Job } from 'bull';
 import { Logger } from '@nestjs/common';
 import * as path from 'path';
-import * as isReachable from 'is-reachable';
+import * as ping from 'net-ping';
+import * as dns from 'dns';
 
 export default async function(job: Job) {
   const logger = new Logger(
@@ -9,10 +10,36 @@ export default async function(job: Job) {
   );
   logger.log(`${job.data.description} ${job.id}`);
 
-  const reachable = await isReachable(job.data.host);
-  if (reachable) {
-    return;
-  } else {
-    throw new Error(`${job.data.host} is not reachable`);
-  }
+  const ip = await new Promise((resolve, reject) =>
+    dns.lookup(job.data.host, (err, address) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(address);
+      }
+    })
+  );
+
+  return new Promise(async (resolve, reject) => {
+    const options = {
+      retries: 0
+    };
+
+    const session = ping.createSession(options);
+
+    session.on('error', function(error) {
+      reject(error);
+      session.close();
+    });
+
+    session.pingHost(ip, function(error, target, sent, received) {
+      if (error) {
+        reject(error);
+      } else {
+        var timeTaken = received - sent;
+        resolve(`${target}: ${timeTaken} ms`);
+      }
+      session.close();
+    });
+  });
 }
