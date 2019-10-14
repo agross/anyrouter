6b39fd4780e13f84e8573d43d41a4eebc72c836e
Alexander Groß <agross@therightstuff.de>
2019-10-14 18:07:26 +0200

    Enable bull debugging with arena

--- app/src/main.ts
+++ app/src/main.ts
@@ -4,6 +4,7 @@ import { Logger } from '@nestjs/common';
 import { ConfigService } from './config/config.service';
 import { AppModule } from './app.module';
 import * as path from 'path';
+import Arena from 'bull-arena';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -19,6 +20,23 @@ async function bootstrap() {
   if (config.enableCors) {
     app.enableCors();
   }
+
+  const arena = Arena(
+    {
+      queues: [
+        {
+          name: 'store',
+          hostId: 'local',
+          redis: {
+            port: 6379,
+            host: '127.0.0.1'
+          }
+        }
+      ]
+    },
+    { disableListen: true, basePath: '/arena' }
+  );
+  app.use(arena);
 
   await app.listen(config.httpPort);
 }
