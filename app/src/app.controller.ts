7c100b1279f2243e86ca0591e103ae1a8fae56f0
Alexander Groß <agross@therightstuff.de>
2019-10-14 18:20:51 +0200

    Trim down app controller

--- app/src/app.controller.ts
+++ app/src/app.controller.ts
@@ -1,13 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { Queue } from 'bull';
-import { InjectQueue } from 'nest-bull';
+import { Controller } from '@nestjs/common';
 
 @Controller()
-export class AppController {
-  constructor(@InjectQueue('store') readonly queue: Queue) {}
-
-  @Get(':id')
-  async getJob(@Param('id') id: string) {
-    return await this.queue.getJob(id);
-  }
-}
+export class AppController {}
