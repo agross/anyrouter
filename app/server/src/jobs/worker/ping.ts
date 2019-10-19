443278309060788646a7ef6b76798a443c76bd1c
Alexander Groß <agross@therightstuff.de>
2019-10-19 17:25:33 +0200

    Attempt to make ping more resilient

--- app/server/src/jobs/worker/ping.ts
+++ app/server/src/jobs/worker/ping.ts
@@ -5,6 +5,41 @@ import * as path from 'path';
 import * as dns from 'dns';
 import * as ping from 'net-ping';
 
+const dnsLookup = util.promisify(dns.lookup);
+
+const options = {
+  retries: 0,
+  _debug: false
+};
+
+const session = ping.createSession(options);
+// session.on('error', (error: Error) => {
+//   logger.error(
+//     `${job.data.host} (${ip.address}): ${JSON.stringify(error, null, 2)}`
+//   );
+//   session.close();
+
+//   throw error;
+// });
+
+session.pingHost[util.promisify.custom] = (ip: string) => {
+  return new Promise((resolve, reject) => {
+    session.pingHost(
+      ip,
+      (error: Error, target: string, sent: number, received: number) => {
+        if (error) {
+          reject(error);
+        } else {
+          const rtt = received - sent;
+          resolve({ ip: target, rtt: rtt });
+        }
+      }
+    );
+  });
+};
+
+const pingHost = util.promisify(session.pingHost);
+
 export default async function(job: Job) {
   const logger = new Logger(
     `${path.basename(__filename, path.extname(__filename))} worker`
@@ -12,46 +47,23 @@ export default async function(job: Job) {
   logger.debug(`${job.data.description} job ${job.id}`);
 
   try {
-    var ip: dns.LookupAddress = await util.promisify(dns.lookup)(job.data.host);
+    var ip: dns.LookupAddress = await dnsLookup(job.data.host);
     logger.debug(`${job.data.host}: IP ${ip.address}`);
   } catch (error) {
     logger.error(`${job.data.host}: IP ${error}`);
     throw error;
   }
 
-  return new Promise((resolve, reject) => {
-    const options = {
-      retries: 0,
-      _debug: false
-    };
-
-    const session = ping.createSession(options);
-    session.on('error', (error: Error) => {
-      logger.error(
-        `${job.data.host} (${ip.address}): ${JSON.stringify(error, null, 2)}`
-      );
-      reject(error);
-      session.close();
-    });
-
-    session.pingHost(
-      ip.address,
-      (error: Error, _target: string, sent: number, received: number) => {
-        session.close();
-
-        if (error) {
-          logger.error(
-            `${job.data.host} (${ip.address}) ${JSON.stringify(error, null, 2)}`
-          );
-          reject(error);
-        } else {
-          const rtt = received - sent;
-          const result = { ip: ip, rtt: rtt };
-
-          logger.debug(`${job.data.host}: ${JSON.stringify(result, null, 2)}`);
-          resolve(result);
-        }
-      }
+  try {
+    const result = await pingHost(ip.address);
+    logger.debug(
+      `${job.data.host} (${ip.address}) ${JSON.stringify(result, null, 2)}`
     );
-  });
+    return result;
+  } catch (error) {
+    logger.error(
+      `${job.data.host} (${ip.address}) ${JSON.stringify(error, null, 2)}`
+    );
+    throw error;
+  }
 }
